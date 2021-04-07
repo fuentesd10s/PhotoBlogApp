ed921a4b32223f47be01ef6b7043019d02b2009d
Gonzalo <fuentesgonzalo@live.com>
2021-04-06 22:03:50 -0300

    First commit

---
+++ app/src/main/java/com/fuentescreations/photoblogapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fuentescreations.photoblogapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fuentescreations.photoblogapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
